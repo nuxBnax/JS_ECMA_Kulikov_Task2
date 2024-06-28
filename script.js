753eef59f0c68a85294dcc1b29b8dd57ab8c6765
Kulikov Alexander <shouter84@yandex.ru>
2024-06-28 22:56:50 +0300

    updated code

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-// Задание 1. Управление библиотекой книг.
+// Задание 1: "Управление библиотекой книг"
+
+// Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+
+// Свойство title (название) - строка, название книги.
+// Свойство author (автор) - строка, имя автора книги.
+// Свойство pages (количество страниц) - число, количество страниц в книге.
+// Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
+
+// Пример использования класса
+// const book = new Book("BookName1", "Author1", 123);
+// book.displayInfo(); // "Title: BookName1, Author: Author1, Pages: 123"
+
 class Book {
 	constructor (title, author, pages) {
 		this.title = title;
@@ -6,13 +18,25 @@ class Book {
 		this.pages = pages;
 	}
 	displayInfo() {
-		console.log(`Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`);
+		return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`;
 	}
 }
  const book = new Book("Bookname1","Author1",123);
- book.displayInfo();
+ console.log(book.displayInfo());
 
+
+//  Задание 2: "Управление списком студентов"
+//  Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
  
+//  Свойство name (имя) - строка, имя студента.
+//  Свойство age (возраст) - число, возраст студента.
+//  Свойство grade (класс) - строка, класс, в котором учится студент.
+//  Метод displayInfo() - выводит информацию о студенте в консоль.
+ 
+ // Пример использования класса
+//  const student = new Student("John Smith", 16, "10th grade");
+//  student.displayInfo(); // "Name: John Smith, Age: 16, Grade: 10th grade"
+
  class Student {
  	constructor(name, age, grade) {
  		this.name = name;
